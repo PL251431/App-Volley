48abaccc7f5973e1579cbeb1b95e4b8dddd4b3b3
PL <pedrolucasdesousaoliveira@aluno.uespi.br>
2024-12-10 22:16:41 -0300

    .

--- lib/screens/first_screen.dart
+++ lib/screens/first_screen.dart
@@ -12,52 +12,54 @@ class FirstScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xff00ADC3), // Fundo azul
-      body: Row(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          // Lateral esquerda com "TIMES" rotacionado
-          Padding(
-            padding: const EdgeInsets.only(left: 4.0), // Adiciona espaço à esquerda
-            child: Container(
-              color: const Color(0xff00ADC3),
-              child: const Center(
-                child: TextTeams(title: 'TIMES'), // Usa o widget RotatedTime
+        const HeaderWidget(),
+        Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 4.0), // Adiciona espaço à esquerda
+              child: Container(
+                color: const Color(0xff00ADC3),
+                child: const Center(
+                  child: TextTeams(title: 'TIMES'), // Usa o widget RotatedTime
+                ),
               ),
             ),
-          ),
-          // Conteúdo principal
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const HeaderWidget(), // Cabeçalho
-                // Lista de times
-                const Padding(
-                  padding: EdgeInsets.all(5.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TeamRow(teamName: 'Sicranos', playerCount: 3),
-                      TeamRow(teamName: 'Autoconvidados', playerCount: 3),
-                      TeamRow(teamName: 'Ziraldos', playerCount: 4),
-                      TeamRow(teamName: 'Sparrings', playerCount: 5),
-                    ],
+            // Conteúdo principal
+            const Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // Cabeçalho
+                  // Lista de times
+                  Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TeamRow(teamName: 'Sicranos', playerCount: 3),
+                        TeamRow(teamName: 'Autoconvidados', playerCount: 3),
+                        TeamRow(teamName: 'Ziraldos', playerCount: 4),
+                        TeamRow(teamName: 'Sparrings', playerCount: 5),
+                      ],
+                    ),
                   ),
-                ),
-                // Substituímos o rodapé original pelo FooterWidget
-                FooterWidget(
-                  onStartPressed: () {
-                    Navigator.pushNamed(context, '/game'); // Vai para a próxima tela
-                  },
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
-      // Botão flutuante
+          ],
+        ),
+        FooterWidget(
+          onStartPressed: () {
+            Navigator.pushNamed(context, '/game'); // Vai para a próxima tela
+          },
+        ),
+      ]),
       floatingActionButton: FloatingButton(
         onPressed: () {
-          // Ação ao pressionar "+"
         },
       ),
     );
