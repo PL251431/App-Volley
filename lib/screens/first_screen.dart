479ff3379daf75532d3692a1bab060fccff3a8ae
PL <pedrolucasdesousaoliveira@aluno.uespi.br>
2024-12-10 22:32:28 -0300

    ...

--- lib/screens/first_screen.dart
+++ lib/screens/first_screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import '../widgets/team_row.dart';
@@ -25,45 +26,52 @@ class _FirstScreenState extends State<FirstScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xff00ADC3), // Fundo azul
-      body: Row(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          // Lateral esquerda com "TIMES" rotacionado
-          Padding(
-            padding: const EdgeInsets.only(left: 4.0), // Adiciona espaço à esquerda
-            child: Container(
-              color: const Color(0xff00ADC3),
-              child: const Center(
-                child: TextTeams(title: 'TIMES'), // Usa o widget RotatedTime
-              ),
-            ),
-          ),
-          // Conteúdo principal
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const HeaderWidget(), // Cabeçalho
-                // Lista de times
-                const Padding(
-                  padding: EdgeInsets.all(5.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TeamRow(teamName: 'Sicranos', playerCount: 3),
-                      TeamRow(teamName: 'Autoconvidados', playerCount: 3),
-                      TeamRow(teamName: 'Ziraldos', playerCount: 4),
-                      TeamRow(teamName: 'Sparrings', playerCount: 5),
-                    ],
+          const HeaderWidget(),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 4.0), // Adiciona espaço à esquerda
+                child: Container(
+                  color: const Color(0xff00ADC3),
+                  child: const Center(
+                    child:
+                        TextTeams(title: 'TIMES'), // Usa o widget RotatedTime
                   ),
                 ),
-                // Substituímos o rodapé original pelo FooterWidget
-                FooterWidget(
-                  onStartPressed: () {
-                    Navigator.pushNamed(context, '/game'); // Vai para a próxima tela
-                  },
+              ),
+              // Conteúdo principal
+              const Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // Cabeçalho
+                    // Lista de times
+                    Padding(
+                      padding: EdgeInsets.all(5.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TeamRow(teamName: 'Sicranos', playerCount: 3),
+                          TeamRow(teamName: 'Autoconvidados', playerCount: 3),
+                          TeamRow(teamName: 'Ziraldos', playerCount: 4),
+                          TeamRow(teamName: 'Sparrings', playerCount: 5),
+                        ],
+                      ),
+                    ),
+                    // Substituímos o rodapé original pelo FooterWidget
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
+          ),
+          FooterWidget(
+            onStartPressed: () {
+              Navigator.pushNamed(context, '/game'); // Vai para a próxima tela
+            },
           ),
         ],
       ),
--- lib/widgets/header_ball.dart
+++ lib/widgets/header_ball.dart
@@ -5,42 +5,41 @@ class HeaderWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 20.0, left: 20.0), // Margem superior e lateral
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center, // Alinha logo e texto verticalmente
-        children: [
-          // Logo
-          Image.asset(
-            'assets/imgs/ball.png', // Caminho do logo
-            height: 60, // Ajuste do tamanho do logo
-          ),
-          const SizedBox(width: 15), // Espaçamento entre logo e texto
-          // Título e subtítulo
-          const Column(
-            crossAxisAlignment: CrossAxisAlignment.start, // Alinhado à esquerda
-            children:  [
-              Text(
-                "Volley",
-                style: TextStyle(
-                  fontFamily: 'ConcertOne',
-                  color: Colors.white,
-                  fontSize: 40, // Tamanho da fonte do título principal
-                  fontWeight: FontWeight.bold,
-                ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,	
+      crossAxisAlignment:
+          CrossAxisAlignment.center, // Alinha logo e texto verticalmente
+      children: [
+        // Logo
+        Image.asset(
+          'assets/imgs/ball.png', // Caminho do logo
+          height: 60, // Ajuste do tamanho do logo
+        ),
+        const SizedBox(width: 15), // Espaçamento entre logo e texto
+        // Título e subtítulo
+        const Column(
+          crossAxisAlignment: CrossAxisAlignment.start, // Alinhado à esquerda
+          children: [
+            Text(
+              "Volley",
+              style: TextStyle(
+                fontFamily: 'ConcertOne',
+                color: Colors.white,
+                fontSize: 40, // Tamanho da fonte do título principal
+                fontWeight: FontWeight.bold,
               ),
-              Text(
-                "do fim de semana",
-                style: TextStyle(
-                  fontFamily: 'ConcertOne',
-                  color: Colors.white,
-                  fontSize: 14, // Tamanho menor para o subtítulo
-                ),
+            ),
+            Text(
+              "do fim de semana",
+              style: TextStyle(
+                fontFamily: 'ConcertOne',
+                color: Colors.white,
+                fontSize: 14, // Tamanho menor para o subtítulo
               ),
-            ],
-          ),
-        ],
-      ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
